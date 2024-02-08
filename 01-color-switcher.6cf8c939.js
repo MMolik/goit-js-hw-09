a3b07a090a0d12ea7b439f35fa006fcc4f114819
MMolik <141867466+MMolik@users.noreply.github.com>
2024-02-08 12:10:06 +0000

    Deploying to gh-pages from @ MMolik/goit-js-hw-09@f61a10cac2cfbea8a64616492059e7d9ec5a3e4f 🚀

---
+++ 01-color-switcher.6cf8c939.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){var n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3);t.disabled=!0,e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1}))}))}();
+//# sourceMappingURL=01-color-switcher.6cf8c939.js.map
