16e55f24c08a3ca0a1ac982e64a0e6089ce53fe4
MMolik <molikos@o2.pl>
2024-02-11 12:41:57 +0100

    step 1

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,6 +2,8 @@
 import flatpickr from "flatpickr";
 // Dodatkowy import stylów
 import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
+import { Report } from 'notiflix/build/notiflix-report-aio';
 
 // dane do zadania //
 const options = {
@@ -11,6 +13,10 @@ const options = {
     minuteIncrement: 1,
     onClose(selectedDates) {
       console.log(selectedDates[0]);
+      const selectedDate = selectedDates[0];
+      const currentDate = new Date();
+
+      
     },
   };
 
@@ -39,4 +45,51 @@ const options = {
 
     //^^dane do zadania^^^//
 
+
+    const button = (document.querySelector('button[data-start]').disabled = true);
+    button.disabled = true;
+
     
+    if (selectedDates[0] <= currentDate) {
+      // Report.Failure('Error', 'Please choose a date in the future', 'OK');
+      document.querySelector('button[data-start]').disabled = true;
+      Notiflix.Notify.failure('Please choose a date in the future');
+    } else {
+      document.querySelector('[data-start]').disabled = false;
+    };
+
+    let countdownInterval;
+    flatpickr('#datetime-picker', options);
+    document.querySelector('button[data-start]').addEventListener('click', startTimer);
+    function startTimer() {
+      const selectedDate = new Date(
+        document.querySelector('#datetime-picker').value
+      );
+      const currentDate = new Date();
+      if (selectedDate <= currentDate) {
+        Notiflix.Notify.failure('Please choose a date in the future');
+        // Report.Failure('Error', 'Please choose a date in the future', 'OK');
+        return;
+      }
+      updateTimer(selectedDate - currentDate);
+      countdownInterval = setInterval(() => {
+        updateTimer(selectedDate - new Date());
+      }, 1000);
+      document.querySelector('button[data-start]').disabled = true;
+    }
+    function updateTimer(timeDiff) {
+      const { days, hours, minutes, seconds } = convertMs(timeDiff);
+      document.querySelector('[data-days]').textContent = addLeadingZero(days);
+      document.querySelector('[data-hours]').textContent = addLeadingZero(hours);
+      document.querySelector('[data-minutes]').textContent =
+        addLeadingZero(minutes);
+      document.querySelector('[data-seconds]').textContent =
+        addLeadingZero(seconds);
+      if (timeDiff <= 0) {
+        clearInterval(countdownInterval);
+        Report.Success('Success', 'Countdown finished!', 'OK');
+      }
+    }
+    function addLeadingZero(value) {
+      return value < 10 ? `0${value}` : value;
+    }
